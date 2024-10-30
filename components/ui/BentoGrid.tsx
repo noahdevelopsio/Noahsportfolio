f28514714b4ca55107c0f00c21fb6fd82d79e40e
noahdevelops.io <noahbig001@gmail.com>
2024-10-30 18:03:37 +0100

    small change

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -45,7 +45,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        'row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4',
+        'row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4',
         className
       )}
       style={{
@@ -86,7 +86,7 @@ export const BentoGridItem = ({
       <div
         className={cn(
           titleClassName,
-          'group-hover/bento:translate-x-2 transition duratio min-h-40 flex flex-col px-5 p-5 lg:p-10'
+          'group-hover/bento:translate-x-2 transition duration-200 min-h-40 flex flex-col px-5 p-5 lg:p-10'
         )}
       >
         <div className='font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10'>
@@ -95,11 +95,32 @@ export const BentoGridItem = ({
         <div className='font-sans font-bold text-lg lg:text-3xl max-w-96 z-10'>
           {title}
         </div>
-      </div>
+      
 
       {id === 2 && <GridGlobe />}
+      {id === 3 && (
+        <div className='flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2' >
+          <div className='flex flex-col gap-3 lg:gap-8'>
+            {['MS Excel','Power BI','SQL','Python'].map((item) =>(
+              <span key={item} className='py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]' >
+                {item}
+              </span>
+            ))}
+            <span className='py-4 px-3 rounded-lg text-center bg[10132e]'></span>
+          </div>
+          <div className='flex flex-col gap-3 lg:gap-8'>
+            {['MS Excel','Javascript','React.js','Next.js'].map((item) =>(
+              <span key={item} className='py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]' >
+                {item}
+              </span>
+            ))}
+            <span className='py-4 px-3 rounded-lg text-center bg[10132e]'></span>
+          </div>
+        </div>
+      )}
 
-      <div className='group-hover/bento:translate-x-2 transition duration-200'></div>
     </div>
+  </div>
+    
   );
 };
