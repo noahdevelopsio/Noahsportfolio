db9bad234a1adc7c68534df6a0b8bc64b9eddf80
noahdevelops.io <noahbig001@gmail.com>
2024-10-30 18:38:09 +0100

    something small

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -45,7 +45,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        'row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4',
+        'row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]',
         className
       )}
       style={{
@@ -106,15 +106,15 @@ export const BentoGridItem = ({
                 {item}
               </span>
             ))}
-            <span className='py-4 px-3 rounded-lg text-center bg[10132e]'></span>
+            <span className='py-4 px-3 rounded-lg text-center bg-[#10132e]'></span>
           </div>
           <div className='flex flex-col gap-3 lg:gap-8'>
-            {['MS Excel','Javascript','React.js','Next.js'].map((item) =>(
+          <span className='py-4 px-3 rounded-lg text-center bg-[#10132e]'/>
+            {['React.js','Next.js','PHP','MongoDB'].map((item) =>(
               <span key={item} className='py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]' >
                 {item}
               </span>
             ))}
-            <span className='py-4 px-3 rounded-lg text-center bg[10132e]'></span>
           </div>
         </div>
       )}
